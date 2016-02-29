d864f1e5081b7d571efa6dd48064712b8fdd31d0
nesscollege2016 <nesscollege2016@gmail.com>
2016-02-29 10:45:50 +0200

    Stars Matrix Double array of size n

--- src/edu/tomer/ness/Main.java
+++ src/edu/tomer/ness/Main.java
@@ -7,45 +7,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[][] arr = { {1, 2, 3, 1, 10},
-                        {4, 5, 6, 2},
-                        {7, 8, 9} };
+        System.out.println("Enter n (Matrix size):");
+        int n = scanner.nextInt();
 
-        /*
-        int[] a = arr[0]; // {1, 2, 3}
+        String[][] starsMatrix = new String[n][n];
 
-
-        //aside
-        for (int i = 0; i < a.length; i++) {
-            System.out.print(a[i]);
-            System.out.print(i == a.length - 1 ? "": ", ");
-        }
-*/
-
-
-
-        for (int rows = 0; rows < arr.length; rows++) {
-            for (int col = 0; col < arr[rows].length; col++) {
-                System.out.print(arr[rows][col]);
-                System.out.print(col == arr[rows].length - 1 ?"\n" : ", ");
-            }
-        }
-
-        int[][] multTable = new int[10][10];
-//
-        multTable[0][0] = 1;
-        for (int i = 0; i < multTable.length; i++) {
-            for (int j = 0; j < multTable[i].length; j++) {
-                multTable[i][j] = (i + 1) * (j + 1);
-                //System.out.print(multTable[i][j]);
-                System.out.printf("%-4d", multTable[i][j]);
+        for (int i = 0; i < starsMatrix.length; i++) {
+            for (int j = 0; j < starsMatrix[i].length; j++) {
+                starsMatrix[i][j] = "*";
+                System.out.print(starsMatrix[i][j] + " ");
             }
             System.out.println();
         }
 
-        String name = "Tomer";
-        String language = "Java";
-        System.out.printf("Your name is: %s and your language is %s",name, language);
+
+
 
     }
 }
