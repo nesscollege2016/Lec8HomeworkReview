83f9b13ee6d89b5d80b734a90195e787b576f3a2
nesscollege2016 <nesscollege2016@gmail.com>
2016-02-29 10:13:10 +0200

    Mult table with double arrays

--- src/edu/tomer/ness/Main.java
+++ src/edu/tomer/ness/Main.java
@@ -6,28 +6,46 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number to search:");
-        int query = scanner.nextInt();
 
-        int[] numbers = {1, 12, 13, 14, 15, 66,99, 100,101};
+        int[][] arr = { {1, 2, 3, 1, 10},
+                        {4, 5, 6, 2},
+                        {7, 8, 9} };
 
-        int firstIdx = 0;
-        int lastIdx = numbers.length - 1; //the index of the last term
+        /*
+        int[] a = arr[0]; // {1, 2, 3}
 
-        int queryIdx = -1;
-        while (firstIdx <= lastIdx){
-            int middle = (firstIdx + lastIdx) / 2;
-            if (numbers[middle] == query){
-                queryIdx = middle;
-                break;
-            }else if (query > numbers[middle]){
-                firstIdx = middle + 1;
-            }
-            else {
-                lastIdx = middle - 1;
+
+        //aside
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]);
+            System.out.print(i == a.length - 1 ? "": ", ");
+        }
+*/
+
+
+
+        for (int rows = 0; rows < arr.length; rows++) {
+            for (int col = 0; col < arr[rows].length; col++) {
+                System.out.print(arr[rows][col]);
+                System.out.print(col == arr[rows].length - 1 ?"\n" : ", ");
             }
         }
 
-        System.out.println(queryIdx);
+        int[][] multTable = new int[10][10];
+//
+        multTable[0][0] = 1;
+        for (int i = 0; i < multTable.length; i++) {
+            for (int j = 0; j < multTable[i].length; j++) {
+                multTable[i][j] = (i + 1) * (j + 1);
+                //System.out.print(multTable[i][j]);
+                System.out.printf("%-4d", multTable[i][j]);
+            }
+            System.out.println();
+        }
+
+        String name = "Tomer";
+        String language = "Java";
+        System.out.printf("Your name is: %s and your language is %s",name, language);
+
     }
 }
